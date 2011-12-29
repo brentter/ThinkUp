04c6461cfb3aa896c3e1cb851e333ae0bfaf1fea
brentter <brent@brentter.com>
2011-12-29 06:48:48 -0700

    added timeline

---
+++ webapp/timeline/config.js
@@ -0,0 +1,4 @@
+// Configuration
+var installation_url = 'http://stats.socialpeople.info/'; // URL of your ThinkUp installation (include trailing slash)
+var username = 'brentter'; // Username for the Twitter account to display
+var embedly_key = 'f51fb56a2d3d11e1a92e4040d3dc5c07'; // Embed.ly API key
